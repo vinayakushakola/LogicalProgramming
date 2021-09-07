0c90ef9e10d7bdeb1490b5dacf61273d4a8969c4
vinayak ushakola <vinayak.ushakola@gmail.com>
2021-09-07 11:03:07 +0530

    Added Day Of Week Gregorian calendar

---
+++ LogicalProgramming/DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProgramming.DayOfWeek
+{
+    class DayOfWeek
+    {
+        /// <summary>
+        /// Logic for date (Sun, Mon,...Sat)
+        /// </summary>
+        /// <param name="day">Day</param>
+        /// <param name="month">Month</param>
+        /// <param name="year">Year</param>
+        /// <returns>Return day in integer form</returns>
+        public static int GetDayOfWeek(int day, int month, int year)
+        {
+            int y0, x, m0, d0;
+            y0 = year - (14 - month) / 12;
+            x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 400);
+            m0 = month + 12 * ((14 - month) / 12) - 2;
+            d0 = (day + x + (31 * m0) / 12) % 7;
+            return d0;
+        }
+    }
+}
--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -15,7 +15,8 @@ namespace LogicalProgramming
                 {
                     Console.WriteLine("Choose any one option");
                     Console.WriteLine("\t1. Vending Machine");
-                    Console.WriteLine("\t2. Exit");
+                    Console.WriteLine("\t2. Day Of Week");
+                    Console.WriteLine("\t3. Exit");
                     Console.Write("Enter choice = ");
                     int choice = Convert.ToInt32(Console.ReadLine());
                     switch (choice)
@@ -25,10 +26,14 @@ namespace LogicalProgramming
                             CallVendingMachine();
                             break;
                         case 2:
+                            Console.WriteLine("\n\t========== 2. Day Of Week ==========");
+                            CallDayOfWeek();
+                            break;
+                        case 3:
                             Console.WriteLine("\nThank You for visiting");
                             flag = false;
                             break;
-                        case 3:
+                        case 4:
                             Console.WriteLine("Invalid choice");
                             break;
                     }
@@ -61,6 +66,44 @@ namespace LogicalProgramming
             {
                 Console.WriteLine("\tInvalid amount entered");
             }
+        }
+
+        /// <summary>
+        /// Day of week input taking logic
+        /// </summary>
+        private static void CallDayOfWeek()
+        {
+            try
+            {
+                int day, month, year;
+                Console.Write("\tEnter Day = ");
+                day = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\tEnter Month = ");
+                month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\tEnter Year = ");
+                year = Convert.ToInt32(Console.ReadLine());
+                Day d0 = (Day)DayOfWeek.DayOfWeek.GetDayOfWeek(day, month, year);
+                Console.WriteLine("\tOutput:");
+                Console.WriteLine("\tDay = {0}", d0);
+            }
+            catch
+            {
+                Console.WriteLine("\tInvalid date entered");
+            }
+        }
+
+        /// <summary>
+        /// Enum for Days
+        /// </summary>
+        private enum Day
+        {
+            Sunday = 0,
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6
         }
     }
 }
