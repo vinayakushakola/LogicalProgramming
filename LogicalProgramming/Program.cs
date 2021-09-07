10ad2e0e419f134ddc59488cc78ca7085928fcd3
vinayak ushakola <vinayak.ushakola@gmail.com>
2021-09-07 08:53:09 +0530

    Added Vending Machine Logic

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -7,7 +7,19 @@ namespace LogicalProgramming
         static void Main(string[] args)
         {
             Console.WriteLine("=============== Welcome to Logical Programming ===============");
+            Console.WriteLine();
+
+            Console.WriteLine("\t========== Vending Machine ==========\n");
+            CallVendingMachine();
             Console.ReadKey();
+        }
+
+        private static void CallVendingMachine()
+        {
+            int amount;
+            Console.Write("\tEnter an amount to get change = ");
+            amount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\tAmount = {0} \n\tNotes = {1}",amount, VendingMachine.VendingMachine.GetMinimumChange(amount));
         }
     }
 }
---
+++ LogicalProgramming/VendingMachine/VendingMachine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LogicalProgramming.VendingMachine
+{
+    class VendingMachine
+    {
+        private const int ONE_THOUSAND = 1000;
+        private const int FIVE_HUNDRED = 500;
+        private const int ONE_HUNDRED = 100;
+        private const int FIFTY = 50;
+        private const int TEN = 10;
+        private const int FIVE = 5;
+        private const int TWO = 2;
+        private const int ONE = 1;
+
+        public static int GetMinimumChange(int amount)
+        {
+            int minimumNotes = 0;
+            while (amount != 0)
+            {
+                if (amount >= ONE_THOUSAND)
+                {
+                    amount -= ONE_THOUSAND;
+                    minimumNotes++;
+                }
+                else if (amount >= FIVE_HUNDRED)
+                {
+                    amount -= FIVE_HUNDRED;
+                    minimumNotes++;
+                }
+                else if (amount >= ONE_HUNDRED)
+                {
+                    amount -= ONE_HUNDRED;
+                    minimumNotes++;
+                }
+                else if (amount >= FIFTY)
+                {
+                    amount -= FIFTY;
+                    minimumNotes++;
+                }
+                else if (amount >= TEN)
+                {
+                    amount -= TEN;
+                    minimumNotes++;
+                }
+                else if (amount >= FIVE)
+                {
+                    amount -= FIVE;
+                    minimumNotes++;
+                }
+                else if (amount >= TWO)
+                {
+                    amount -= TWO;
+                    minimumNotes++;
+                }
+                else if (amount >= ONE)
+                {
+                    amount -= ONE;
+                    minimumNotes++;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            return minimumNotes;
+        }
+    }
+}
