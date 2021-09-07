80e58e62decdb47e9a927ce70fddd52fa307d4b1
vinayak ushakola <vinayak.ushakola@gmail.com>
2021-09-07 08:04:19 +0530

    Added Welcome Message

---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LogicalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=============== Welcome to Logical Programming ===============");
+            Console.ReadKey();
+        }
+    }
+}
