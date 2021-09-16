87cc02f693c455914840ed816ed5b5d25f1820e6
vinayak ushakola <vinayak.ushakola@gmail.com>
2021-09-16 09:42:44 +0530

    Refactored the code

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace LogicalProgramming
 {
@@ -26,23 +27,23 @@ namespace LogicalProgramming
                     {
                         case 1:
                             Console.WriteLine("\n\t========== 1. Vending Machine ==========");
-                            CallVendingMachine();
+                            clsInputTaking.CallVendingMachine();
                             break;
                         case 2:
                             Console.WriteLine("\n\t========== 2. Day Of Week ==========");
-                            CallDayOfWeek();
+                            clsInputTaking.CallDayOfWeek();
                             break;
                         case 3:
                             Console.WriteLine("\n\t========== 3. Temperature Conversion ==========");
-                            CallTemperatureConvertor();
+                            clsInputTaking.CallTemperatureConvertor();
                             break;
                         case 4:
                             Console.WriteLine("\n\t========== 4. Monthly Payment ==========");
-                            CallMonthlyPayment();
+                            clsInputTaking.CallMonthlyPayment();
                             break;
                         case 5:
                             Console.WriteLine("\n\t========== 4. Square Root ==========");
-                            CallSquareRoot();
+                            clsInputTaking.CallSquareRoot();
                             break;
                         case 6:
                             Console.WriteLine("\nThank You for visiting");
@@ -59,160 +60,9 @@ namespace LogicalProgramming
                     Console.WriteLine("Try Again!");
                 }
                 Console.WriteLine("\n==============================================================\n");
+                Thread.Sleep(2000);
             }
             Console.ReadKey();
-        }
-
-        /// <summary>
-        /// Vending Machine input taking logic
-        /// </summary>
-        private static void CallVendingMachine()
-        {
-            int amount;
-            bool IsAmountValid;
-            Console.Write("\tEnter an amount to get change = ");
-            IsAmountValid = int.TryParse(Console.ReadLine(), out amount);
-            Console.WriteLine("\tOutput:");
-            if (IsAmountValid)
-            {
-                int totalNotes = 0;
-                if (VendingMachine.VendingMachine.GetMinimumChange(amount))
-                {
-                    Console.WriteLine("\tAmount = {0}", amount);
-                    for (int i = 0; i < 9; i++)
-                    {
-                        if (VendingMachine.VendingMachine.noteCounter[i] != 0)
-                        {
-                            Console.WriteLine("\t" + VendingMachine.VendingMachine.notes[i] + " Notes = " +
-                                VendingMachine.VendingMachine.noteCounter[i]);
-                            totalNotes += VendingMachine.VendingMachine.noteCounter[i];
-                        }
-                    }
-                    Console.WriteLine("\tTotal Notes = {0}", totalNotes);
-                }
-            }
-            else
-            {
-                Console.WriteLine("\tInvalid amount entered");
-            }
-        }
-
-        /// <summary>
-        /// Day of week input taking logic
-        /// </summary>
-        private static void CallDayOfWeek()
-        {
-            try
-            {
-                int day, month, year;
-                Console.Write("\tEnter Day = ");
-                day = Convert.ToInt32(Console.ReadLine());
-                Console.Write("\tEnter Month = ");
-                month = Convert.ToInt32(Console.ReadLine());
-                Console.Write("\tEnter Year = ");
-                year = Convert.ToInt32(Console.ReadLine());
-                Day d0 = (Day)DayOfWeek.DayOfWeek.GetDayOfWeek(day, month, year);
-                Console.WriteLine("\tOutput:");
-                Console.WriteLine("\tDay = {0}", d0);
-            }
-            catch
-            {
-                Console.WriteLine("\tInvalid date entered");
-            }
-        }
-
-        /// <summary>
-        /// Temperature Conversion input taking logic
-        /// </summary>
-        private static void CallTemperatureConvertor()
-        {
-            Console.Write("\tEnter temprature = ");
-            int temperature;
-            bool IsValidNo = int.TryParse(Console.ReadLine(), out temperature);
-            if (IsValidNo)
-            {
-                Console.WriteLine("\tChoose Conversion");
-                Console.WriteLine("\t\t1. Celsius to Fahrenheit");
-                Console.WriteLine("\t\t2. Fahrenheit to Celsius");
-                Console.Write("\tEnter choice = ");
-                int choice = 0;
-                choice = int.Parse(Console.ReadLine());
-                Console.WriteLine("\tOutput:");
-                switch (choice)
-                {
-                    case 1:
-                        int tempInFr = TemperatureConversion.TemperatureConvertor.CelsiusToFahrenheit(temperature);
-                        Console.WriteLine("\tCelsius {0} to Fahrenheit {1}", temperature, tempInFr);
-                        break;
-                    case 2:
-                        int tempInCe = TemperatureConversion.TemperatureConvertor.FahrenheitToCelsius(temperature);
-                        Console.WriteLine("\tFahrenheit {0} to Celcius {1}", temperature, tempInCe);
-                        break;
-                    default:
-                        Console.WriteLine("\tInvalid choice");
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("\tInvalid number entered");
-            }
-        }
-
-        /// <summary>
-        /// Monthly payment input taking logic
-        /// </summary>
-        private static void CallMonthlyPayment()
-        {
-            try
-            {
-                Console.Write("\tEnter principle amount = ");
-                int Principleamount = int.Parse(Console.ReadLine());
-                Console.Write("\tEnter No of year = ");
-                int NoOfYear = int.Parse(Console.ReadLine());
-                Console.Write("\tEnter Rate = ");
-                int Rate = int.Parse(Console.ReadLine());
-                double payment = MonthlyPayment.MonthlyPayment.CalculateMontlyPayment(Principleamount, NoOfYear, Rate);
-                Console.WriteLine("\tMonthly Payment = {0}", payment);
-            }
-            catch
-            {
-                Console.Write("Invalid value");
-            }
-        }
-
-        /// <summary>
-        /// Square Root input taking logic
-        /// </summary>
-        private static void CallSquareRoot()
-        {
-            int numSq;
-            Console.Write("\tEnter number = ");
-            bool IsValidNo = int.TryParse(Console.ReadLine(), out numSq);
-            Console.WriteLine("Output: ");
-            if (IsValidNo)
-            {
-                double squareRoot = SquareRoot.SquareRoot.GetSquareRoot(numSq);
-                Console.WriteLine("\tSquare Root = {0}", squareRoot);
-            }
-            else
-            {
-                Console.WriteLine("\tInvalid number entered");
-            }
-        }
-
-        /// <summary>
-        /// Enum for Days
-        /// </summary>
-        private enum Day
-        {
-            Sunday = 0,
-            Monday = 1,
-            Tuesday = 2,
-            Wednesday = 3,
-            Thursday = 4,
-            Friday = 5,
-            Saturday = 6
         }
     }
 }
--- LogicalProgramming/VendingMachine/VendingMachine.cs
+++ LogicalProgramming/VendingMachine/VendingMachine.cs
@@ -5,7 +5,7 @@ namespace LogicalProgramming.VendingMachine
     class VendingMachine
     {
         public static int[] notes = new int[] { 2000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
-        public static int[] noteCounter = new int[9];
+        public static int[] noteCounter = new int[10];
 
         /// <summary>
         /// Logic for Minimum Notes Change
@@ -14,7 +14,7 @@ namespace LogicalProgramming.VendingMachine
         /// <returns> No of Notes </returns>
         public static bool GetMinimumChange(int amount)
         {
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i <= 9; i++)
             {
                 if (amount >= notes[i])
                 {
